33800268aca320b5de27aaada4a999bec17ec7b1
Sagar <dev.sagartmg@gmail.com>
2024-03-26 16:51:38 +0545

    components

--- react-base/src/main.jsx
+++ react-base/src/main.jsx
@@ -1,72 +1,79 @@
 /* ecmascript module(react) vs common js (express) */
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./main.css";
 
 let title = "React.js";
-let description =
-  "lorem ipsum dolor, sit amet consectetur adipisicing ipsum dolor, sit amet consectetur adipisicin ipsum dolor, sit amet consectetur adipisicin ipsum dolor, sit amet consectetur adipisicin ipsum dolor, sit amet consectetur adipisicin elit.";
+let description = "Lorem ipsum dolor sit amet consectetur adipisicin";
 
 let todos = [
   {
     title: "html",
     description:
       "lorem ipsum dolor, sit amet consectetur adipisicing ipsum dolor",
-    status: "completed",
+    status: true,
   },
   {
     title: "css",
     description:
       "lorem ipsum dolor, sit amet consectetur adipisicing ipsum dolor",
-    status: true,
+    status: false,
   },
   {
-    title: "reac",
+    title: "react",
     description:
       "lorem ipsum dolor, sit amet consectetur adipisicing ipsum dolor",
     status: false,
   },
 ];
 
+
+/* functional Component */
+function Todo() {
+  return (
+    <div className="todo">
+      <h3>
+        React 
+      </h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio explicabo nulla sint quia commodi assumenda aliquam amet illo soluta eligendi, voluptas totam sed laboriosam cupiditate a autem repudiandae, voluptate alias.</p>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>
-      <h1>{title}</h1>
-      <hr />
-      <p>{description}</p>
-      <h2>Todos</h2>
+    <h1>{title}</h1>
+    <p>{description}</p>
 
-      <div className="todos">
-        <div class="todo">
-          <h3>
-            {todos[0].title} ( {todos[0].status} )
-          </h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, ut
-            repellat? Quis, adipisci voluptatem! Accusantium aut consequatur
-            quisquam nobis nulla debitis. Totam voluptates, aspernatur quos
-            impedit corrupti adipisci amet! Laboriosam.
-          </p>
-        </div>
-        <div class="todo">
-          <h3>html ( complted )</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, ut
-            repellat? Quis, adipisci voluptatem! Accusantium aut consequatur
-            quisquam nobis nulla debitis. Totam voluptates, aspernatur quos
-            impedit corrupti adipisci amet! Laboriosam.
-          </p>
-        </div>
-        <div class="todo">
-          <h3>html ( complted )</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, ut
-            repellat? Quis, adipisci voluptatem! Accusantium aut consequatur
-            quisquam nobis nulla debitis. Totam voluptates, aspernatur quos
-            impedit corrupti adipisci amet! Laboriosam.
-          </p>
-        </div>
+    <h2>Todos</h2>
+
+    {/* props in component */}
+    <div className="todos">
+      <Todo />
+      <Todo />
+      <Todo />
+
+      {/* <div className="todo">
+        <h3>
+          {todos[0].title} ( {todos[0].status ? "complted ✔ " : "pending ❌"} )
+        </h3>
+        <p>{todos[0].description}</p>
       </div>
+
+      <div className="todo">
+        <h3>
+          {todos[1].title} ( {todos[1].status ? "comploted ✔ " : "pending ❌"} )
+        </h3>
+        <p>{todos[1].description}</p>
+      </div>
+
+      <div className="todo">
+        <h3>
+          {todos[2].title} ( {todos[2].status ? "comploted ✔ " : "pending ❌"} )
+        </h3>
+        <p>{todos[2].description}</p>
+      </div> */}
     </div>
   </React.StrictMode>
 );
