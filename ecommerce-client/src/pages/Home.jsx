b28da4ffd8fe720f4acf32045c8422fba4d91724
JobLessGod <iamjoblessgod@gmail.com>
2024-04-24 13:14:05 +0545

    added Login page for mobile only
    Not foe other device, its not responsive

--- ecommerce-client/src/pages/Home.jsx
+++ ecommerce-client/src/pages/Home.jsx
@@ -12,6 +12,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const settings = {
     dots: true,
@@ -55,10 +56,9 @@ export default function Home() {
       .then((res) => {
         console.log(res);
         setProducts(res.data.data);
+        isLoading(false);
       })
-      .catch(err=>{
-
-      })
+      .catch((err) => {});
   }, []);
 
   return (
@@ -81,19 +81,20 @@ export default function Home() {
             />
           );
         })}
-        {/* <Banner background={"bg-banner-1"} />
+        {/* <Banner background={"bg-banner-1"} />   
         <Banner background={"bg-banner-2"} />
         <Banner background={"bg-banner-3"} /> */}
       </Slider>
+      {/* {isLoading && (
+          <>
+            <Skeleton className="h-[250px]" />
+            <Skeleton className="h-[250px]" />
+            <Skeleton className="h-[250px]" />
+            <Skeleton className="h-[250px]" />
+          </>
+        )}, */}
 
-      <div className="container grid grid-cols-4 gap-4">
-        <Skeleton className="h-[250px]" />
-        <Skeleton className="h-[250px]" />
-        <Skeleton className="h-[250px]" />
-        <Skeleton className="h-[250px]" />
-      </div>
-
-      <div className="container grid   gap-4 py-[116px] sm:py-[130px] md:grid-cols-2 md:py-[148px] lg:grid-cols-4 lg:py-[166px] xl:py-[188px] xxl:py-[210px]">
+      <div className="container grid gap-4 py-[116px] sm:py-[130px] md:grid-cols-2 md:py-[148px] lg:grid-cols-4 lg:py-[166px] xl:py-[188px] xxl:py-[210px]">
         {products.map((el) => {
           return <Product name={el.name} price={el.price} image={el.image} />;
         })}
